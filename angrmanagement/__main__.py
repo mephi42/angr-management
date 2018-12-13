844f196a49e3568b4e2843b4f5e78f898ffcdde4
Ilya Leoshkevich <iii@linux.ibm.com>
2018-12-13 11:12:11 +0100

    Fix PySide2 installation hint
    pyside didn't work for me, since it required Python 3.4 or older.

--- angrmanagement/__main__.py
+++ angrmanagement/__main__.py
@@ -11,7 +11,7 @@ def check_dependencies():
         import PySide2
     except ImportError:
         sys.stderr.write("Cannot find the PySide2 package. You may install it via pip:\n" +
-                         "    pip install pyside2\n")
+                         "    pip install PySide2\n")
         return False
 
     try:
